68f9ed65b3a7af6c8812656ce99246afad1aebf8
josephhess <readysteadyco@gmail.com>
2018-08-23 11:18:54 -0700

    cat carousel works

---
+++ cat_carousel/index.js
@@ -0,0 +1,10 @@
+const hero_image = () => {
+  $('.thumbnails a').on('click',function(e) {   
+    const image_source = $(this).find('img').attr('src');
+    const image_alt = $(this).find('img').attr('alt');
+    $('div.hero img').attr('src', image_source);
+    $('div.hero alt').attr('alt', image_alt);
+  });
+};
+
+$(hero_image);
